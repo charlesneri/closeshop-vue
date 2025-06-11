8a6548325f8f7764d886afce23fc57a71a5e978d
Charles <charlesneri0201@gmail.com>
2025-06-11 17:57:36 +0800

    first commit-m

---
+++ src/main.js
@@ -0,0 +1,30 @@
+//import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const app = createApp(App)
+
+// Components
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+   icons: {
+    defaultSet: 'mdi',
+  },
+})
+app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
